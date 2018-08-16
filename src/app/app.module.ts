eba19f6b3b9ba28db6c71b9a38b1a5472ad55e10
William Langroudi <william.langroudi@IkenDEV.local>
2018-08-16 09:37:15 +0100

    Initial setup and components, Bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
